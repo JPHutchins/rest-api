03f0425df191023d9df7c73a16e39248ec4585a4
J.P. Hutchins <jphutchins@gmail.com>
2021-01-18 12:56:06 -0800

    Alphabetize routes by function name

--- library/__init__.py
+++ library/__init__.py
@@ -27,56 +27,38 @@ def create_app(config) -> Flask:
             add_users(db)
             add_books(db)
 
-        @app.route(f"{API_BASE_PATH}/users", methods=["GET"])
-        def users():
-            """Get the list of users."""
-            users = db.session.query(models.User).all()
-            users_json = [
-                {
-                    "book_limit": user.book_limit,
-                    "books_checked_out": len(user.checkouts),
-                    "email": user.email,
-                    "firstname": user.firstname,
-                    "lastname": user.lastname,
-                    "uri": f"/users/{user.id}",
-                }
-                for user in users
-            ]
-            return jsonify(users_json)
+        @app.route(f"{API_BASE_PATH}/books", methods=["GET"])
+        def books():
+            """Get the list of books in the library."""
+            books = db.session.query(models.Book).all()
+            return jsonify(
+                [
+                    {
+                        "book_id": book.id,
+                        "genre": book.genre,
+                        "title": book.title,
+                    }
+                    for book in books
+                ]
+            )
 
-        @app.route(f"{API_BASE_PATH}/users/<int:user_id>", methods=["GET"])
-        def user(user_id: int = None):
-            """Get the user by id."""
-            user = db.session.query(models.User).filter_by(id=user_id).first()
-            if not user:
-                return jsonify({"error": f"User {user_id} does not exist."}), 400
-            user_info = {
-                "book_limit": user.book_limit,
-                "books_checked_out": len(user.checkouts),
-                "email": user.email,
-                "firstname": user.firstname,
-                "lastname": user.lastname,
-                "checkouts": [
-                    {"book_id": checkout.book_id, "due_date": checkout.due_date}
-                    for checkout in user.checkouts
-                ],
-            }
-            return jsonify(user_info)
-
-        @app.route(f"{API_BASE_PATH}/users/<int:user_id>/checkouts", methods=["GET"])
-        def user_checkouts(user_id: int = None):
-            """Get the list of books checked out by a user."""
-            user = db.session.query(models.User).filter_by(id=user_id).first()
-            if not user:
-                return jsonify({"error": f"User {user_id} does not exist."}), 400
+        @app.route(f"{API_BASE_PATH}/checkouts", methods=["GET"])
+        def checkouts():
+            """Get the list of books currently checked out."""
+            checkouts = (
+                db.session.query(models.Checkout)
+                .order_by(models.Checkout.due_date)
+                .all()
+            )
             checkouts_json = [
                 {
                     "book_id": checkout.book_id,
-                    "out_date": checkout.out_date,
                     "due_date": checkout.due_date,
+                    "out_date": checkout.out_date,
                     "title": checkout.book.title,
+                    "user_id": checkout.user_id,
                 }
-                for checkout in user.checkouts
+                for checkout in checkouts
             ]
             return jsonify(checkouts_json)
 
@@ -115,21 +97,6 @@ def create_app(config) -> Flask:
             response.headers["Location"] = f"{API_BASE_PATH}/users/{user_id}/checkouts"
             return response
 
-        @app.route(f"{API_BASE_PATH}/books", methods=["GET"])
-        def books():
-            """Get the list of books in the library."""
-            books = db.session.query(models.Book).all()
-            return jsonify(
-                [
-                    {
-                        "book_id": book.id,
-                        "genre": book.genre,
-                        "title": book.title,
-                    }
-                    for book in books
-                ]
-            )
-
         @app.route(f"{API_BASE_PATH}/books/<int:book_id>", methods=["DELETE"])
         def delete_book(book_id: int):
             """Delete the book by id if possible."""
@@ -164,23 +131,56 @@ def create_app(config) -> Flask:
             )
             return response, 400
 
-        @app.route(f"{API_BASE_PATH}/checkouts", methods=["GET"])
-        def checkouts():
-            """Get the list of books currently checked out."""
-            checkouts = (
-                db.session.query(models.Checkout)
-                .order_by(models.Checkout.due_date)
-                .all()
-            )
+        @app.route(f"{API_BASE_PATH}/users/<int:user_id>", methods=["GET"])
+        def user(user_id: int = None):
+            """Get the user by id."""
+            user = db.session.query(models.User).filter_by(id=user_id).first()
+            if not user:
+                return jsonify({"error": f"User {user_id} does not exist."}), 400
+            user_info = {
+                "book_limit": user.book_limit,
+                "books_checked_out": len(user.checkouts),
+                "email": user.email,
+                "firstname": user.firstname,
+                "lastname": user.lastname,
+                "checkouts": [
+                    {"book_id": checkout.book_id, "due_date": checkout.due_date}
+                    for checkout in user.checkouts
+                ],
+            }
+            return jsonify(user_info)
+
+        @app.route(f"{API_BASE_PATH}/users", methods=["GET"])
+        def users():
+            """Get the list of users."""
+            users = db.session.query(models.User).all()
+            users_json = [
+                {
+                    "book_limit": user.book_limit,
+                    "books_checked_out": len(user.checkouts),
+                    "email": user.email,
+                    "firstname": user.firstname,
+                    "lastname": user.lastname,
+                    "uri": f"/users/{user.id}",
+                }
+                for user in users
+            ]
+            return jsonify(users_json)
+
+        @app.route(f"{API_BASE_PATH}/users/<int:user_id>/checkouts", methods=["GET"])
+        def user_checkouts(user_id: int = None):
+            """Get the list of books checked out by a user."""
+            user = db.session.query(models.User).filter_by(id=user_id).first()
+            if not user:
+                return jsonify({"error": f"User {user_id} does not exist."}), 400
             checkouts_json = [
                 {
                     "book_id": checkout.book_id,
-                    "due_date": checkout.due_date,
                     "out_date": checkout.out_date,
+                    "due_date": checkout.due_date,
                     "title": checkout.book.title,
-                    "user_id": checkout.user_id,
                 }
-                for checkout in checkouts
+                for checkout in user.checkouts
             ]
             return jsonify(checkouts_json)
 
